4095faf4f9e3af29b07535b8ea4402eb4771e54b
Sahil Singh Fartyal <57360832+Sahil4405@users.noreply.github.com>
2022-09-03 13:13:06 +0530

    Create 546A - Soldier and Bananas.cpp

---
+++ CP/Striver CP Sheet/Implementation/546A - Soldier and Bananas.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll                 long long int
+#define vi                 vector<int>
+#define pll                pair<ll,ll>
+#define ump                unordered_map
+#define mp                 make_pair
+#define pb                 push_back
+#define ff                 first
+#define ss                 second 
+#define all(n)             n.begin(),n.end()
+#define loop(i,a,b)        for(int i = a;i < b; i++)
+#define looprev(i,a,b)     for(i = a; i > b; i--)
+#define ps(x,y)            fixed<<setprecision(y)<<x 
+#define FIO                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    FIO; 
+
+    ll k,n,w;
+    cin>>k>>n>>w;
+
+    ll total_cost = (k * w * (w + 1))/2;
+
+    if(n >= total_cost)
+        cout<<"0";
+    else{
+        ll final_ans = abs(n - total_cost);
+        cout<<final_ans;
+    }
+    
+
+    return 0;
+}
